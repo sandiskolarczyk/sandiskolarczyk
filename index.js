c9a62e0ffee8f223e3c1322108f21e36c5430601
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-05-01 12:39:01 +0100

    Create readme template; Move imgs to a folder

---
+++ index.js
@@ -0,0 +1,4 @@
+// 1) Create a variable to reference the README.template.md file
+// 2) Make the request to The Office API
+// 3) Look through the README.template.md content and replace the static patterns ({office_quote} & {office_character}) with the dynamic result of the API request
+// 4) Create/replace the contents of the README.md file with the updated README.template.md reference variable.
